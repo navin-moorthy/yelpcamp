61bd6172b7a0b9aaf5e1b5a4a7b39d5f4f8a5593
Navin <navin007.a@gmail.com>
2019-03-02 18:41:56 +0000

    Env var updated for mongo db

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ var campgroundRoutes = require("./routes/campgrounds");
 var authRoutes = require("./routes/index");
     
 // Package Initializations
-mongoose.connect("mongodb+srv://atlasnavi:3u7TREmYj726PGme@yelpcamptest-4xjxz.mongodb.net/yelp_camp?retryWrites=true", {useNewUrlParser: true});    
-// mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});    
+mongoose.connect("process.env.DATABASE_URL", {useNewUrlParser: true});    
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
