cb3260b18b478afbf202564532436762dc88840c
Navin <navin007.a@gmail.com>
2019-03-02 17:50:28 +0000

    Added the Clout Mongo DB Atlas

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var campgroundRoutes = require("./routes/campgrounds");
 var authRoutes = require("./routes/index");
     
 // Package Initializations
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});    
+mongoose.connect("mongodb+srv://atlasnavi:3u7TREmYj726PGme@yelpcamptest-4xjxz.mongodb.net/yelp_camp?retryWrites=true", {useNewUrlParser: true});    
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
