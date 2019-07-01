3a95e1c031769e9c963d850449ec66fb7e375b3f
NavinNavi19 <navin007.a@gmail.com>
2019-07-01 16:38:02 +0000

    Prettified

--- routes/index.js
+++ routes/index.js
@@ -8,44 +8,47 @@ var passport = require("passport");
 // ====================================================
 
 // show register form
-router.get("/register", function(req, res){
-    res.render("register", {page: 'register'});
+router.get("/register", function(req, res) {
+  res.render("register", { page: "register" });
 });
 
-router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-        if(err){
-            req.flash("error", err.message);
-            return res.redirect("back");
-        }
-        passport.authenticate("local")(req, res, function(){
-            req.flash("success", "Welcome to YelpCamp " + user.username);
-            res.redirect("/campgrounds");
-        });
+router.post("/register", function(req, res) {
+  var newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err) {
+      req.flash("error", err.message);
+      return res.redirect("back");
+    }
+    passport.authenticate("local")(req, res, function() {
+      req.flash("success", "Welcome to YelpCamp " + user.username);
+      res.redirect("/campgrounds");
     });
+  });
 });
 
 // show login form
-router.get("/login", function(req, res){
-    res.render("login", {page: "login"});
+router.get("/login", function(req, res) {
+  res.render("login", { page: "login" });
 });
 
-router.post("/login", passport.authenticate("local",
-    {
-        successRedirect: "/campgrounds",
-        failureRedirect: "/login",
-        successFlash: "Welcome Back to Yelpcamp",
-        failureFlash: true
-    }), function(req, res){
-        res.send("Post accepted");
-});
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login",
+    successFlash: "Welcome Back to Yelpcamp",
+    failureFlash: true
+  }),
+  function(req, res) {
+    res.send("Post accepted");
+  }
+);
 
 // logout the user
-router.get("/logout", function(req, res){
-    req.logout();
-    req.flash("error", "See you soon!!")
-    res.redirect("/campgrounds");
+router.get("/logout", function(req, res) {
+  req.logout();
+  req.flash("error", "See you soon!!");
+  res.redirect("/campgrounds");
 });
 
 module.exports = router;
