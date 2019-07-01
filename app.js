0aa1feb553d50e2ba722ebcef2edf60e91873c14
NavinNavi19 <navin007.a@gmail.com>
2019-07-01 16:37:00 +0000

    Dependencies Updated

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
 // To use Google API Key
-require('dotenv').config();
+require("dotenv").config();
 // Node Packages
-var express    = require("express"),
-    app        = express(),
-    bodyParser = require("body-parser"),
-    mongoose   = require("mongoose"),
-    flash      = require("connect-flash"),
-    passport   = require("passport"),
- localStrategy = require("passport-local"),
-    User       = require("./models/user"),
-    seedDB     = require("./seed"),
-methodOverride = require("method-override");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  localStrategy = require("passport-local"),
+  User = require("./models/user"),
+  seedDB = require("./seed"),
+  methodOverride = require("method-override");
 
 // Requiring Routes
 var commentRoutes = require("./routes/comments");
 var campgroundRoutes = require("./routes/campgrounds");
 var authRoutes = require("./routes/index");
-    
+
 // Package Initializations
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});    
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 // seedDB() // Seed the database
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
-    secret: 'idontknow',
+app.use(
+  require("express-session")({
+    secret: "idontknow",
     resave: false,
-    saveUninitialized: false,
-}));
+    saveUninitialized: false
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -41,19 +43,17 @@ app.use(flash());
 
 // Local Variables
 app.locals.moment = require("moment");
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    res.locals.danger = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.danger = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
-
-
 
 // Routes
 // Home Route
 app.get("/", function(req, res) {
-    res.render("landing");
+  res.render("landing");
 });
 
 app.use("/", authRoutes);
@@ -62,8 +62,8 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 // Server Listen
 app.listen(process.env.PORT, process.env.IP, function(err) {
-    if(err){
-        console.log(err);
-    }
-    console.log("I am at your service");
+  if (err) {
+    console.log(err);
+  }
+  console.log("I am at your service");
 });
--- seed.js
+++ seed.js
@@ -1,42 +1,48 @@
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
-var Comment = require("./models/comment")
+var Comment = require("./models/comment");
 
 var data = [
-    {
-        name: "Acorn Oaks Campground", 
-        image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=400" , 
-        description: "Extended kindness trifling remember he confined outlived if. Assistance sentiments yet unpleasing say. Open they an busy they my such high. An active dinner wishes at unable hardly no talked on. Immediate him her resolving his favourite. Wished denote abroad at branch at."
-    },
-    {
-        name: "Atwood Lake Campground", 
-        image: "https://images.pexels.com/photos/1687845/pexels-photo-1687845.jpeg?auto=compress&cs=tinysrgb&h=400", 
-        description: "In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed."
-    },
-    {
-    name: "Gordon's Campground", 
-    image: "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=400", 
-    description: "Fog everywhere. Fog up the river, where it flows among green aits and meadows; fog down the river, where it rolls deified among the tiers of shipping and the waterside pollutions of a great (and dirty) city. Fog on the Essex marshes, fog on the Kentish heights. Fog creeping into the cabooses of collier-brigs."
-    }
+  {
+    name: "Acorn Oaks Campground",
+    image:
+      "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=400",
+    description:
+      "Extended kindness trifling remember he confined outlived if. Assistance sentiments yet unpleasing say. Open they an busy they my such high. An active dinner wishes at unable hardly no talked on. Immediate him her resolving his favourite. Wished denote abroad at branch at."
+  },
+  {
+    name: "Atwood Lake Campground",
+    image:
+      "https://images.pexels.com/photos/1687845/pexels-photo-1687845.jpeg?auto=compress&cs=tinysrgb&h=400",
+    description:
+      "In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed."
+  },
+  {
+    name: "Gordon's Campground",
+    image:
+      "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=400",
+    description:
+      "Fog everywhere. Fog up the river, where it flows among green aits and meadows; fog down the river, where it rolls deified among the tiers of shipping and the waterside pollutions of a great (and dirty) city. Fog on the Essex marshes, fog on the Kentish heights. Fog creeping into the cabooses of collier-brigs."
+  }
 ];
 
-function seedDB(){
-    // Remove all CampGrounds
-    Campground.remove({}, function(err){
-        if(err){
-            console.log(err);
+function seedDB() {
+  // Remove all CampGrounds
+  Campground.remove({}, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Removed the campgrounds");
+      // Remove all Comments
+      Comment.remove({}, function(err) {
+        if (err) {
+          console.log(err);
         } else {
-            console.log("Removed the campgrounds");
-            // Remove all Comments
-            Comment.remove({}, function(err){
-                if(err){
-                    console.log(err);
-                } else {
-                   console.log("Removed all the comments");
-                }
-            });
+          console.log("Removed all the comments");
         }
-    });
+      });
+    }
+  });
 }
 
 module.exports = seedDB;
